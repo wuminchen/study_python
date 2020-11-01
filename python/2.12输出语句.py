062d22241fb39b4c93b268e66e654ea94d7e8eaa
WUMINCHEN <chen794350566@gmail.com>
2020-11-01 13:21:29 +0800

    学习python2020年11月1日13:21:27

---
+++ python/2.12输出语句.py
@@ -0,0 +1,11 @@
+#Python里使用print内置函数，输出内容
+
+#print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+ #sep 参数表示输出，每个值之间用那种字符进行分割，默认为空格。
+ #end 当执行完一个print以后，接下来要输出的字符。默认'\n'表示换行
+ #
+
+print('hello','good','hi',sep='+',end = '------')
+print('我是问问')
+
+
