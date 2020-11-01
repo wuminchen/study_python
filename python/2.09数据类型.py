95c9351056e2126b2b1a9bd6e5b09aa6cf79555e
WUMINCHEN <chen794350566@gmail.com>
2020-11-01 12:25:58 +0800

    学习了数据类型

---
+++ python/2.09数据类型.py
@@ -0,0 +1,38 @@
+a = "hello world"   #a 称之为变量
+print(a)
+
+b = 34
+print(b)
+
+c = True
+print(c)
+
+# 数据类型：
+#在python数据又各自对应类型
+
+#数字类型
+print(45)   #int整数类型
+print(3.1415)   #float浮点类型
+print(2 ** 4 )  #complex复数类型
+
+#字符串类型：
+#python里的使用单引号或者双引号来包裹
+print('你好，世界')
+print("56" )
+
+#布尔类型：用来表示真假/对错
+#只有两种类型
+print(4 > 3)    #对
+print(1 > 5)    #错
+
+#列表类型
+names = ['a','b','c','b','d']
+
+#字典类型
+person = {'name':'Cate','age':18,'addr':'北京','height':'185'}
+
+#元组类型
+nums = (1,5,23,213,5,3)
+
+#集合类型
+x = {9,'hello','hi',True}
